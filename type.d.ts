6f25180c826f656a35f50485dc4f6d9440c7883c
chuck <chuck@chihai.me>
2023-08-16 18:14:11 +0800

    :label: json数据格式变更

--- type.d.ts
+++ type.d.ts
@@ -90,117 +90,124 @@ export type PostItem = {
   };
   followCount: number;
 };
+
+export type UserInfo = {
+  userAttr: {
+    nickname: string;
+    username: string;
+    encryptedUsername: string;
+    encryptedHeadImage: string;
+    city: string;
+    sex: number;
+    country: string;
+    province: string;
+    spamflag: number;
+    spamflag2: number;
+  };
+  finderUser: {
+    finderUsername: string;
+    nickname: string;
+    headImgUrl: string;
+    coverImgUrl: string;
+    spamFlag: number;
+    acctType: number;
+    authIconType: number;
+    adminNickname: string;
+    feedsCount: number;
+    fansCount: number;
+    categoryFlag: string;
+    acctStatus: number;
+    uniqId: string;
+    isMasterFinder: boolean;
+  };
+  datacenterEntarnce: number;
+  envInfo: {
+    cdnHostList: Array<string>;
+    spareCdnHostList: Array<string>;
+    productEnv: number;
+    cdnHost: string;
+    uploadVersion: number;
+  };
+  livesvrEnter: number;
+  switchInfo: {
+    commentSelection: number;
+    asyncClipPostSwitch: number;
+    originalsoundSwitch: number;
+    personalmsgFlag: number;
+    selectionSwitch: number;
+    editCommentAuthSwitch: number;
+    userAttrReplayPrivilege: number;
+    canCreateReplayTransition: number;
+    enableH265Upload: number;
+  };
+  entranceInfo: {
+    commentManage: number;
+    pullstreamliveManage: number;
+    s1sFamousEntrance: number;
+    authEntrance: number;
+    adEntrance: number;
+    liveShopEntrance: number;
+    livePurchaseEntrance: number;
+    liveIncomeEntrance: number;
+    liveEcdataEntrance: number;
+    eventManageEntrance: number;
+    commentSelectionEntrance: number;
+    liveroomManageEntrance: number;
+    liveNoticeManageEntrance: number;
+    collectionEntrance: number;
+    originalEntranceInfo: {
+      contactAdditionalFlag: number;
+      originalWarning: string;
+    };
+    personalColumnManageEntrance: number;
+    shortTitleEntrance: number;
+    collectionEntranceInfo: {
+      audioCollectionEntranceInfo: number;
+    };
+    musicEntranceInfo: {
+      musicManagerEntrance: number;
+      takedownSongButtonEntrance: number;
+      takedownAlbumButtonEntrance: number;
+      bindButtonEntrance: number;
+    };
+    replayEntrance: number;
+    promotionEntrance: number;
+    liveleadsEntrance: number;
+    mpUrlPostEntrance: number;
+    memberEntranceInfo: {
+      memberManagerEntrance: number;
+    };
+    openMenu: number;
+    tencentVideoPostEntrance: number;
+    audioEntranceInfo: {
+      audioManagerEntrance: number;
+    };
+    thirdpartyPushStreamEntranceInfo: number;
+    openUpdateWwkf: number;
+  };
+  isRealName: boolean;
+  authInfo: {
+    authIconType: number;
+    authVerifyIdentity: number;
+    currentYearAuthTimes: number;
+    simpleAuthStatus: number;
+    authAnnualReview: {
+      status: number;
+    };
+  };
+  signature: string;
+  proxyUid: string;
+  txvideoOpenId: string;
+};
+
 export type QueryResult = Array<{
   user: {
-    userAttr: {
-      nickname: string;
-      username: string;
-      encryptedUsername: string;
-      encryptedHeadImage: string;
-      city: string;
-      sex: number;
-      country: string;
-      province: string;
-      spamflag: number;
-      spamflag2: number;
-    };
-    finderUser: {
-      finderUsername: string;
-      nickname: string;
-      headImgUrl: string;
-      coverImgUrl: string;
-      spamFlag: number;
-      acctType: number;
-      authIconType: number;
-      adminNickname: string;
-      feedsCount: number;
-      fansCount: number;
-      categoryFlag: string;
-      acctStatus: number;
-      uniqId: string;
-      isMasterFinder: boolean;
-    };
-    datacenterEntarnce: number;
-    envInfo: {
-      cdnHostList: Array<string>;
-      spareCdnHostList: Array<string>;
-      productEnv: number;
-      cdnHost: string;
-      uploadVersion: number;
-    };
-    livesvrEnter: number;
-    switchInfo: {
-      commentSelection: number;
-      asyncClipPostSwitch: number;
-      originalsoundSwitch: number;
-      personalmsgFlag: number;
-      selectionSwitch: number;
-      editCommentAuthSwitch: number;
-      userAttrReplayPrivilege: number;
-      canCreateReplayTransition: number;
-      enableH265Upload: number;
-    };
-    entranceInfo: {
-      commentManage: number;
-      pullstreamliveManage: number;
-      s1sFamousEntrance: number;
-      authEntrance: number;
-      adEntrance: number;
-      liveShopEntrance: number;
-      livePurchaseEntrance: number;
-      liveIncomeEntrance: number;
-      liveEcdataEntrance: number;
-      eventManageEntrance: number;
-      commentSelectionEntrance: number;
-      liveroomManageEntrance: number;
-      liveNoticeManageEntrance: number;
-      collectionEntrance: number;
-      originalEntranceInfo: {
-        contactAdditionalFlag: number;
-        originalWarning: string;
-      };
-      personalColumnManageEntrance: number;
-      shortTitleEntrance: number;
-      collectionEntranceInfo: {
-        audioCollectionEntranceInfo: number;
-      };
-      musicEntranceInfo: {
-        musicManagerEntrance: number;
-        takedownSongButtonEntrance: number;
-        takedownAlbumButtonEntrance: number;
-        bindButtonEntrance: number;
-      };
-      replayEntrance: number;
-      promotionEntrance: number;
-      liveleadsEntrance: number;
-      mpUrlPostEntrance: number;
-      memberEntranceInfo: {
-        memberManagerEntrance: number;
-      };
-      openMenu: number;
-      tencentVideoPostEntrance: number;
-      audioEntranceInfo: {
-        audioManagerEntrance: number;
-      };
-      thirdpartyPushStreamEntranceInfo: number;
-      openUpdateWwkf: number;
-    };
-    isRealName: boolean;
-    authInfo: {
-      authIconType: number;
-      authVerifyIdentity: number;
-      currentYearAuthTimes: number;
-      simpleAuthStatus: number;
-      authAnnualReview: {
-        status: number;
-      };
-    };
-    signature: string;
-    proxyUid: string;
-    txvideoOpenId: string;
+    errCode: number;
+    errMsg: string;
+    data: UserInfo;
   };
-  snapshoots: Array<{
-    snapshoot: number;
+  snapshots: Array<{
+    snapshot: number;
     list: Array<PostItem>;
   }>;
 }>;
@@ -211,4 +218,4 @@ export type WeChatChannelsApiResponse<T> = {
   data: T;
   errCode: number;
   errMsg: string;
-}
+};
